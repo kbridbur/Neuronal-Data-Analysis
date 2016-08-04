4d0b030a97fc19a08f0f28cb854af5f349a96b83
Kyle Bridburg <kbridbur@mit.edu>
2016-08-04 17:49:38 -0400

    Add OnlyFullSeconds option

---
+++ GetDataFromFile.m
@@ -0,0 +1,5 @@
+function [DataArray] = GetDataFromFile(file)
+%Takes file and parses it into data that the other functions can use
+
+end
+
---
+++ TagBehaviors.m
@@ -0,0 +1,7 @@
+function [Behaviors] = TagBehaviors(InputList)
+%Using locations of mouse body and head at various timepoints creates a
+%timestamped list of behaviors in chronological order
+
+
+end
+

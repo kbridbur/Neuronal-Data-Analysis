30ae3106c5c353e9fb65d42dc9c8d4adf8a76a2e
Kyle Bridburg <kbridbur@mit.edu>
2016-08-05 22:07:59 -0400

    Add Exist checks for variables

--- AnalyzeDataFiles.m
+++ AnalyzeDataFiles.m
@@ -1,6 +1,24 @@
-function [LeftAvgActivity, CenterAvgActivity, RightAvgActivity, LeftNumSpikes, CenterNumSpikes, RightNumSpikes] = AnalyzeDataFiles(FiberPhotometryFile, MouseLocationFile, FiberPhotometryHz, MouseVisualizationHz, ReductionLayers, PeakCutoff, WindowSize, StartTime, EndTime)
+function [LeftAvgActivity, CenterAvgActivity, RightAvgActivity, LeftNumSpikes, CenterNumSpikes, RightNumSpikes] = AnalyzeDataFiles(FiberPhotometryFile, MouseLocationFile, FiberPhotometryHz, MouseVisualizationHz, StartTime, EndTime, ReductionLayers, PeakCutoff, WindowSize)
 %This function should run all the other ones and return all the information
 %needed, the user should only have to run this one function
+MouseLocationMatrix = ;%load some stuff
+
+%If some variables haven't been entered set them to default values
+if exist(ReductionLayers) < 2
+  ReductionLayers = 0;
+end
+if exist(WindowSize) < 2
+  WindowSize = 3;
+end
+if exist(PeakCutoff) <2
+  PeakCutoff = 1;
+end
+if exist(StartTime)
+ StartTime = 0; 
+end
+if exist(EndTime)
+ EndTime = Length(MouseLocationMatrix)*MouseVisualizationHz;
+end
 [LeftAvgActivity, CenterAvgActivity, RightAvgActivity, LeftNumSpikes, CenterNumSpikes, RightNumSpikes, LeftTimeSpent, CenterTimeSpent, RightTimeSpent] = RelateSideToNeuronActivity(...);
 
 end
